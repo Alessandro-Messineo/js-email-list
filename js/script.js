fd7fadcd51f365dc248251fb2bb52263e681ac73
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-09-15 16:25:31 +0200

    aggiunta di css

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 // output
 const elementiEmailList = document.getElementById("email-list");
+// bottone 
 const btn = document.getElementById("btn-email");
-
+// costante per decidere quante volte iterare il for
 const numEmail = 10;
 
+// azione per quando viene cliccato il bottone
 btn.addEventListener("click", () => {
+    // elemento della lista che si svuota
     elementiEmailList.innerHTML = "";
+    // ciclo per richiedere le email
     for (let i = 0; i < numEmail; i++) {
+        // richiesta ajax
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then(element => {
                 const email = element.data.response;
-                console.log(email);
-                elementiEmailList.innerHTML += `<li>${email}</li>`;
+                // inserimento degli elementi nella pagina
+                elementiEmailList.innerHTML += `<li class="list-group-item">${email}</li>`;
             })
             .catch(error => {
                 console.error(error)
