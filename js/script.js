2a7c9e02e0921f4ad39757dba48a2e69e146e44d
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-09-15 15:55:52 +0200

    aggiunta bottone per richiedere email

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 // output
 const elementiEmailList = document.getElementById("email-list");
+const btn = document.getElementById("btn-email");
 
 const numEmail = 10;
 
-for (let i = 0; i < numEmail; i++) {
-
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(element => {
-            const email = element.data.response;
-            console.log(email);
-            elementiEmailList.innerHTML += `<li>${email}</li>`;
-        })
-        .catch(error => {
-            console.error(error)
-        })
+btn.addEventListener("click", () => {
+    elementiEmailList.innerHTML = "";
+    for (let i = 0; i < numEmail; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(element => {
+                const email = element.data.response;
+                console.log(email);
+                elementiEmailList.innerHTML += `<li>${email}</li>`;
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
 }
+)
